4b6e8e7aeccb20f11cac36cca6a05543e29240e0
nhunggit <nhung97hkmats@gmail.com>
2019-10-01 17:37:33 +0700

    update

--- app/src/main/java/com/example/myappmusic/ListSongFragment.java
+++ app/src/main/java/com/example/myappmusic/ListSongFragment.java
@@ -1,103 +1,66 @@
 package com.example.myappmusic;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaMetadataRetriever;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Filter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListSongFragment extends Fragment {
-
-    private RecyclerView mRecyclerView;
-    protected SongAdapter songAdapter;
-    private TextView mNameSong, mArtist;
-    private ImageView mdisk;
-    private ConstraintLayout constraintLayout;
-    private ImageButton mClickPlay;
-    private SharedPreferences mSharedReferences;
-    private String Shared_References_Name="com.example.myappmusic";
-    private int position;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    View inflate;
+    TextView nameSong;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.list_song_recyclerview,container, false);
-        initView(view);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
-        setHasOptionsMenu(true);
-        mSharedReferences=this.getActivity().getSharedPreferences(Shared_References_Name, Context.MODE_PRIVATE);
-        position=mSharedReferences.getInt("position",3);
-        mNameSong.setText(mSharedReferences.getString("nameArtist","Name Artist"));
-
-        if(!mSharedReferences.getString("path","").equals(""))
-            if(imageArtist(mSharedReferences.getString("path",""))!=null){
-                mdisk.setImageBitmap(imageArtist(mSharedReferences.getString("path","")));
-            }else
-                mdisk.setImageResource(R.drawable.default_cover_art);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        inflate=inflater.inflate(R.layout.list_song_recyclerview,container,false);
+        nameSong=(TextView)inflate.findViewById(R.id.namePlaySong);
+        recyclerView=(RecyclerView)inflate.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ArrayList<Song> arrayList=new ArrayList<>();
+        SongAdapter songAdapter=new SongAdapter( arrayList,getContext());
+        recyclerView.setAdapter(songAdapter);
+        return inflate;
     }
-    public Bitmap imageArtist(String path){
-        Log.d("path",path+"//");
-        MediaMetadataRetriever mediaMetadataRetriever=new MediaMetadataRetriever();
-        mediaMetadataRetriever.setDataSource(path);
-        byte[] data=mediaMetadataRetriever.getEmbeddedPicture();
-        if(data!=null){
-            return BitmapFactory.decodeByteArray(data,0,data.length);
+    void createListSong(){
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+        String[] projection = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.DURATION
+        };
+
+        Cursor cursor = this.managedQuery(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                null);
+
+        private List<String> songs = new ArrayList<String>();
+        while(cursor.moveToNext()){
+            songs.add(cursor.getString(0) + "||" + cursor.getString(1) + "||" +   cursor.getString(2) + "||" +   cursor.getString(3) + "||" +  cursor.getString(4) + "||" +  cursor.getString(5));
         }
-        return null;
-    }
-    void initView(View view){
-        mRecyclerView=view.findViewById(R.id.recyclerview);
-        mArtist=view.findViewById(R.id.Artist);
-        mNameSong=view .findViewById(R.id.namePlaySong);
-        mNameSong.setSelected(true);
-        constraintLayout=view.findViewById(R.id.constraintLayout);
-        mClickPlay=view.findViewById(R.id.play);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater=getActivity().getMenuInflater();
-        inflater.inflate(R.menu.main,menu);
-
-        MenuItem menuItem=menu.findItem(R.id.app_bar_search);
-        SearchView searchView=(SearchView)menuItem.getActionView();
-        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                songAdapter.
-                return false;
-            }
-        });
     }
 }
+
+
--- app/src/main/java/com/example/myappmusic/SongAdapter.java
+++ app/src/main/java/com/example/myappmusic/SongAdapter.java
@@ -1,128 +1,73 @@
 package com.example.myappmusic;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.text.Normalizer;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
 
-public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.WordViewHolder> {
 private ArrayList<Song> mListSong=new ArrayList<>();
 private ArrayList<Song> mSong;
 private LayoutInflater mInflater;
 private Context mcontext;
-private OnClickItemView monClickItemView;
 
-    public SongAdapter(OnClickItemView monClickItemView, Context context) {
-        mInflater=LayoutInflater.from(mcontext);
-        mcontext=context;
-        this.monClickItemView = monClickItemView;
+    public SongAdapter(ArrayList<Song> listBaihat, Context context) {
+        this.mListSong = listBaihat;
+        //this.layout=layout;
+        this.mcontext = context;
     }
-
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view=mInflater.inflate(R.layout.row_recyclerview, parent, false);
-        return new ViewHolder(view);
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View itemview=inflater.inflate(R.layout.row_recyclerview,parent,false);
+        return new WordViewHolder(itemview,this);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        if(mSong!=null){
-            Song current=mSong.get(position);
-            holder.mStt.setText(current.getId()+"");
-            holder.mNameSong.setText(current.getTitle());
-            SimpleDateFormat formatTime=new SimpleDateFormat("mm:ss");
-            holder.mHours.setText(formatTime.format(current.getDuration()));
-
-            final Song finalCurrent=current;
-            holder.mConstraintLayout.setOnClickListener(new View.OnClickListener(){
-
-                @Override
-                public void onClick(View v) {
-                    monClickItemView.clickItem(finalCurrent);
-                }
-            });
-        }
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        holder.mStt.setText(mListSong.get(position).getId()+"");
+        holder.mNameSong.setText(mListSong.get(position).getTitle());
+        holder.mHour.setText(mListSong.get(position).getDuration());
     }
 
     @Override
     public int getItemCount() {
-        if(mSong!=null)
-            return  mSong.size();
-        else
-            return 0;
+        return mListSong.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView mNameSong, mHours;
-        ImageButton mMore;
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView mStt;
-        ConstraintLayout mConstraintLayout;
-        public ViewHolder(@NonNull View itemView) {
+        TextView mNameSong;
+        TextView mHour;
+        ImageButton mMore;
+        final SongAdapter songAdapter;
+
+
+        public WordViewHolder(@NonNull View itemView,SongAdapter songAdapter) {
             super(itemView);
-            mConstraintLayout=itemView.findViewById(R.id.constraintLayoutItem);
-            mNameSong=itemView.findViewById(R.id.namesong);
-            mHours=itemView.findViewById(R.id.hours);
-            mStt=itemView.findViewById(R.id.stt);
-            mMore=itemView.findViewById(R.id.more);
-        }
-    }
+            this.songAdapter=songAdapter;
+            mStt=(TextView)itemView.findViewById(R.id.stt);
+            mNameSong=(TextView)itemView.findViewById(R.id.namesong);
+            mHour=(TextView)itemView.findViewById(R.id.hours);
+            mMore=(ImageButton) itemView.findViewById(R.id.more);
 
-    interface OnClickItemView{
-        void clickItem(Song song);
-    }
-    public Filter getFiler(){
-        return filter;
-    }
-    private Filter filter=new Filter() {
-        @Override
-        protected FilterResults performFiltering(CharSequence charSequence) {
-            ArrayList<Song> filterList=new ArrayList<>();
-            Log.d("ok",charSequence+"//");
-            if(charSequence==null||charSequence.length()==0){
-                filterList.addAll(mListSong);
-            }else{
-                String filterPattern=unAccent(charSequence.toString().toLowerCase().trim());
-
-                for(Song song:mListSong){
-                    if(unAccent(song.getTitle().toLowerCase()).contains(filterPattern)){
-                        filterList.add(song);
-                    }
-                }
-            }
-
-            FilterResults results=new FilterResults();
-            results.values=filterList;
-            return results;
         }
 
         @Override
-        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-            mSong.clear();
-            mSong.addAll((Collection<? extends Song>) filterResults.values);
-            notifyDataSetChanged();
+        public void onClick(View v) {
+            int mPosition =getLayoutPosition();
+            Song element=mListSong.get(mPosition);
+            mListSong.set(mPosition,element);
+            songAdapter.notifyDataSetChanged();
         }
-    };
-    public static String unAccent(String s){
-        String temp= Normalizer.normalize(s, Normalizer.Form.NFD);
-        Pattern pattern=Pattern.compile("\\p{InCOMBINING_DIACRITICAL_MARKS}+");
-        return pattern.matcher(temp).replaceAll("").replaceAll("Đ","D").replace("đ","d");
     }
-
 }
